c59e7f5a6f38dd1070ed031e009ffb0921b6cf48
bnwin <brandon.noel.win@gmail.com>
2020-11-11 11:44:18 -0800

    button and mcluhan reading

---
+++ button/theMedium.js
@@ -0,0 +1,48 @@
+
+let theBody = document.querySelector('body');
+let theButton = document.querySelector("button");
+let theName = document.querySelector("p");
+
+let prevButton = document.querySelector('button[name="prev"]');
+let nextButton = document.querySelector('button[name]="next"]');
+
+let firstName = document.querySelector('div[class="first"]');
+
+let firstNames = ["Angry", "Barefoot", "Bashful", "Beefy", "Bendy", "Big", "Blue", "Bony", "Brave", "Bronze", "Busy", "Calm", "Cheerful", "Chilly", "Clean", "Comical", "Cool", "Crazy", "Creepy", "Cuddly", "Curious", "Dangerous", "Dizzy", "Fast", "Fearless", "Fierce", "Friendly", "Funny", "Gentle", "Giant", "Golden", "Greedy", "Green", "Grey", "Grumpy", "Happy", "Hungry", "Hyper", "Icy", "Incredible", "Invisible", "Jumpy", "Lazy", "Little", "Lone", "Loud", "Lucky", "Mad", "Magic", "Maroon", "Massive", "Messy", "Mighty", "Moody", "Muddy", "Neat", "Nervous", "Nice", "Noisy", "Orange", "Perfect", "Popular", "Prickly", "Purple", "Quick", "Quiet", "Red", "Rough", "Scary", "Serious", "Shaggy", "Shaky", "Shield", "Shifty", "Shiny", "Shoeless", "Short", "Shy", "Silly", "Silver", "Skinny", "Sleepy", "Sleepy", "Slippery", "Small", "Smart", "Sneaky", "Speedy", "Sporty", "Spotted", "Squeezy", "Sticky", "Strange", "Striped", "Super", "Tall", "Thirsty", "Tiny", "Tough", "Trusty", "Wild", "White", "Yellow", "Young", "Zippy", "Zany"]
+let lastNames = ["Ant", "Axe", "Bean", "Bear", "Bee", "Beetle", "Belly", "Berry", "Biker", "Bird", "Bite", "Boa", "Bones", "Boot", "Brain", "Bubbles", "Bug", "Burger", "Cactus", "Carrot", "Catfish", "Cheetah", "Chick", "Chicken", "Claw", "Cloud", "Clown", "Club", "Comet", "Coyote", "Crab", "Crown", "Crumb", "Crush", "Diamond", "Diver", "Dolphin", "Donut", "Dragon", "Drummer", "Eagle", "Eel", "Eye", "Fang", "Feather", "Fire", "Fish", "Flame", "Flipper", "Fly", "Flyer", "Foot", "Fox", "Gamer", "Ghost", "Glove", "Goose", "Grape", "Hamburger", "Hammer", "Hawk", "Heart", "Hero", "Hippo", "Hopper", "Horse", "Ice", "Icicle", "Jester", "Joker", "Jumper", "Kid", "Knuckle", "Laser", "Leaf", "Leopard", "Lightning", "Lion", "Lizard", "Lobster", "Melon", "Monster", "Moon", "Mosquito", "Ninja", "Noodle", "Octopus", "Onion", "Owl", "Panda", "Paw", "Peanut", "Pear", "Pelican", "Penguin", "Plug", "Popper", "Raptor", "Rider", "Ring", "Rock", "Runner", "Scorpion", "Seal", "Seagull", "Shadow", "Shark", "Shell", "Singer", "Sinker", "Skull", "Skunk", "Sky", "Sneeze", "Snowball", "Socks", "Speck", "Spider", "Spinner", "Sponge", "Star", "Starfish", "Stomper", "Storm", "Sun", "Sword", "Thunder", "Tiger", "Toes", "Tomato", "Tooth", "Tornado", "Tummy", "Turkey", "Turtle", "Typhoon", "Walker", "Whale"]
+
+let i = 0;
+
+theButton.addEventListener('click', randomizeClick);
+prevButton.addEventListener('click', prevClick);
+nextButton.addEventListener('click', nextClick);
+
+
+function randomizeClick(){
+    console.log("click!!!");
+    theName.textContent = firstNames[Math.floor(Math.random() * (firstNames.length - 1))] + " " + lastNames[Math.floor(Math.random() * (lastNames.length - 1))];
+}
+
+function prevClick(){
+    console.log("prev clicked");
+    if (i > 0){
+        i = i-1;
+    }else{
+        i = firstNames.length - 1;
+    }
+
+    firstName.textContent = firstNames[i];
+}
+
+function nextClick(){
+    console.log("next clicked");
+    if (i < firstNames.length - 1){
+        i = i+1;
+    }else{
+        i = 0;
+    }
+    firstName.textContent = firstNames[i];
+}
+
+
+
--- mcluhan/script.js
+++ mcluhan/script.js
@@ -5,7 +5,7 @@ nextButton.addEventListener('click', next);
 
 let theTxt = document.querySelector('p');
 
-let textArray = ["The McLuhan reading was an interesting read for class. I particularly enjoyed reading about how McLuhan described technology as mediums through which information is conveyed.", 
+let textArray = ["The McLuhan reading was an interesting read for class. I particularly enjoyed reading about how McLuhan described technology as mediums through which information is conveyed.",
                 "The medium that McLuhan refers to is defined as the device or way that individuals receive information. McLuhan emphasizes the technology or medium as the core catalyst of change rather than the content or media that it is transmitting.",
                 "Examples of such a medium include the Internet, the printing press, the mobile phone and the television. All of these technologies, in their own time, spurred great change, whereas the content that they produced was not the main catalyst.",
                 "I also personally really liked the formatting and the style of the text with its spacing and formatting throughout the book. It was a really engaging and interesting way to present ideas, and it wasn't really boring to read the whole thing."]
@@ -29,6 +29,6 @@ function next(){
     } else {
         i = 0;
     }
-    
+
     theTxt.textContent = textArray[i];
 }
